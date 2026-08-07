c9ccca154e80cadca9adab26a4dc18ca49e58d21
fedeRizzi04 <fedeksus@gmail.com>
2026-08-07 18:11:28 +0200

    minor

--- src/visualsimplex/__init__.py
+++ src/visualsimplex/__init__.py
@@ -1,6 +1,7 @@
 from visualsimplex.value_objects import Constraint, ConstraintSense, Expression, Objective, OptimizationSense, Term, VarKind, VarDomain, Variable
 from visualsimplex.lp_problem import CanonicalFormLPProblem, LPProblem
-
+from visualsimplex.tableau import Tableau, TableauRow
+from visualsimplex.rules import EnteringCandidate, EnteringVariableRule, LeavingCandidate, LeavingVariableRule, dantzig_rule, bland_rule, minimum_ratio_rule
 
 __all__ = [
     "Constraint",
@@ -14,4 +15,13 @@ __all__ = [
     "Variable",
     "CanonicalFormLPProblem",
     "LPProblem",
+    "Tableau",
+    "TableauRow", 
+    "EnteringCandidate",
+    "EnteringVariableRule",
+    "LeavingCandidate",
+    "LeavingVariableRule",
+    "minimum_ratio_rule",
+    "dantzig_rule",
+    "bland_rule"
 ]
